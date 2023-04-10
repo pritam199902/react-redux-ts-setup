785c0af628a1e36b1771abe84756b80ced1ea87e
Pritam Das <49154874+pritam199902@users.noreply.github.com>
2023-04-10 22:41:08 +0530

    Update counter.action.ts

--- src/store/counter.action.ts
+++ src/store/counter.action.ts
@@ -21,22 +21,6 @@ export const onIncrementCounter = () => async (dispatch: AppDispatch) => {
 }
 
 
-
-// export const onDecrementCounter = () => async (dispatch: AppDispatch) => asyncActionHandler(async () => {
-//     console.log('async call initiated....');
-//     dispatch({
-//         // type: counterActionsType.loading_counter
-//     } as TLoadingAction)
-//     await new Promise((resolve) => {
-//         setTimeout(() => {
-//             resolve(true)
-//         }, 4000);
-//     })
-//     dispatch({
-//         type: counterActionsType.decrement_counter
-//     } as TDecrementAction);
-//     console.log("async call complete");
-// })
 export const onDecrementCounter = () => async (dispatch: AppDispatch) => {
     console.log('async call initiated....');
     dispatch({
